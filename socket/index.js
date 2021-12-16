75ada5818f15a7ca750f274bb92c6b2bffff04b9
Bishal Poudel <poudelb172@gmail.com>
2021-12-16 21:40:07 +0545

    socket server connected

--- socket/index.js
+++ socket/index.js
@@ -1,1 +1,19 @@
-console.log("Lets Go")
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors: {
+        //this means our react application from port 3000
+        //can reach our socket server
+        origin: "http://localhost:3000",
+    },
+})
+
+io.on("connection", (socket) => {
+    console.log("Someone has been connected")
+
+    socket.on("disconnect", () => {
+        console.log("Someone has been disconnected")
+    })
+});
+
+io.listen(5000)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,9 @@
 import './login.css'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './Components/Home/Home.js';
+import { io } from 'socket.io-client'
 
 function App() {
   const [userx, setUserx] = useState("")
@@ -12,6 +13,11 @@ function App() {
     localStorage.setItem('userNotify', userx)
     window.location.reload()
   }
+
+  useEffect(() => {
+    const socket = io("http://localhost:5000");
+    console.log(socket)
+  }, []);
 
   return (
     <>
