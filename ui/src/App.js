1841429f73c74730eff35addc30b76c5fc741a47
Bishal Poudel <poudelb172@gmail.com>
2021-12-16 18:22:56 +0545

    user login and saved into localStorage

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,27 @@
-
+import './login.css'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import './App.css';
-import Login from './Components/Login/Login';
+import React, { useState } from 'react';
 
 function App() {
+  const [userx, setUserx] = useState("")
+  const user = localStorage.getItem('userNotify')
+
+  const getUser = () => {
+    localStorage.setItem('userNotify', userx)
+  }
+
   return (
     <>
-      <Login />
+      {
+        !user ?
+          (<div className="loginContainer">
+            <input onChange={(e) => setUserx(e.target.value)}
+              type="text" placeholder="Enter Username"
+              name="" id="" />
+            <button onClick={getUser}>Get Started <ArrowForwardIosIcon /> </button>
+          </div>) : (<div>{user}</div>)
+      }
     </>
   );
 }
